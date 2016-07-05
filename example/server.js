7e7a39299e84035263d938bdb21cfe5c0125f90d
James Halliday <mail@substack.net>
2016-07-04 17:15:11 -0700

    print a message when the hacked loader starts

--- example/server.js
+++ example/server.js
@@ -10,9 +10,15 @@ var server = http.createServer(function (req, res) {
   console.log(req.method, req.url)
   if (req.url === '/worker.js' && times++ > 0) {
     res.setHeader('content-type', 'text/javascript')
-    res.end(`self.addEventListener('fetch', function (ev) {
-      ev.respondWith(new Response('HAX'))
-    })`)
+    res.end(`
+      console.log('HACKED WORKER LOADED')
+      self.addEventListener('fetch', function (ev) {
+        ev.respondWith(new Response('HAX'))
+      })
+    `)
+  } else if (req.url === '/worker.js') {
+    res.setHeader('service-worker-max-age', 1000*60*60*24*365*100) // 100 years
+    st(req, res)
   } else st(req, res)
 })
 server.listen(Number(process.argv[2]))
